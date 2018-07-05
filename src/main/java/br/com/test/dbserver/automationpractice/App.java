01b58bf13a3f50f8b68367b09ea5287b6f3969eb
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2018-07-04 21:00:52 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/test/dbserver/automationpractice/App.java
@@ -0,0 +1,13 @@
+package br.com.test.dbserver.automationpractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
